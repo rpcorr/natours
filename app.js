8c512b381dcc9ca60510c111d6d5868ff1cb0b14
Ronan Corr <ronan_corr@hotmail.com>
2022-07-13 13:21:31 -0400

    update a specifc tour

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.get('/api/v1/tours/:id', (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
 
+  // see if there is a tour
   //if (id > tours.length) {
   if (!tour) {
     return res.status(404).json({
@@ -74,6 +75,24 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+// update tour
+app.patch('/api/v1/tours/:id', (req, res) => {
+  // see if there is a tour
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
 });
 
 const port = 3000;
