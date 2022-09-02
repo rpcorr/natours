bce0f58e5cd8da575991ea0839ce6071dc9dd347
Ronan Corr <ronan_corr@hotmail.com>
2022-09-02 10:30:49 -0400

    update the deleteTour function to delete data from MongoDB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -86,9 +86,20 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+  
 };
