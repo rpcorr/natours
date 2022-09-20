c790294fa760bcec1a19a61fb05d2dd3ea8115d7
Ronan Corr <ronan_corr@hotmail.com>
2022-09-20 06:53:39 -0400

    add sorting to the query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -9,22 +9,31 @@ exports.getAllTours = async (req, res) => {
     console.log(req.query);
 
     // BUILD QUERY
-    // 1) Filtering
+    // 1A) Filtering
     const queryObj = {...req.query}; // creates a copy of the query object
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     // remove excludedFields from queryObj
     excludedFields.forEach(el => delete queryObj[el]);
 
-    // 2) Advance filtering
+    // 1B) Advance filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    console.log(JSON.parse(queryStr));
     
     // { difficulty: 'easy', duration: { $gte: 5 } }
     // { difficulty: 'easy', duration: { gte: '5' } }
     // gte, gt, lte, lt
     
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if(req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+      // sort('price, ratingsAverage')  // sorting by two criteria
+    } else {
+      // add a default sort criteron
+      query = query.sort('-createdAt');
+    }
     
     // EXECUTE QUERY
     const tours = await query;
