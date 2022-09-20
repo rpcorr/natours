e6bb23ce33a4580a387fb5efe2d0f1fea3215480
Ronan Corr <ronan_corr@hotmail.com>
2022-09-20 13:19:49 -0400

    add field limiting; and hide createdAt field from results

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -26,13 +26,21 @@ exports.getAllTours = async (req, res) => {
     let query = Tour.find(JSON.parse(queryStr));
 
     // 2) Sorting
-    if(req.query.sort) {
+    if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
       // sort('price, ratingsAverage')  // sorting by two criteria
     } else {
       // add a default sort criteron
       query = query.sort('-createdAt');
+    }
+
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
     }
     
     // EXECUTE QUERY
