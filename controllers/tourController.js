ad4350735157f86e6b3f9449e9bedf5ec5ca29c1
Ronan Corr <ronan_corr@hotmail.com>
2022-09-27 03:13:15 -0400

    aggregation pipeline; matching & grouping create router tour-stats

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -10,15 +10,17 @@ exports.aliasTopTours = (req, res, next) => {
   req.query.sort = '-ratingsAverage,price';
   req.fields = 'name,price,ratingAverage,summary,difficulty';
   next();
-}
+};
 
 exports.getAllTours = async (req, res) => {
   try {
-
     // EXECUTE QUERY
-    const features = new APIFeatures(Tour.find(), req.query).filter().sort().limitFields().paginate();
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
     const tours = await features.query;
-
 
     // console.log(req.query, queryObj);
 
@@ -26,7 +28,6 @@ exports.getAllTours = async (req, res) => {
     //   duration: 5,
     //   difficulty: 'easy'
     // });
-
 
     //console.log(req.requestTime);
 
@@ -121,6 +122,48 @@ exports.deleteTour = async (req, res) => {
       status: 'success',
       data: null,
     });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      { 
+        $match: { ratingsAverage: { $gte: 4.5 } }
+     },
+     {
+        $group: {
+          //_id: 'null',
+         //_id: '$difficulty',
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1},
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        }  
+     },
+     {
+      $sort: { avgPrice: 1  }
+    }
+    // {
+    //   $match: { _id: { $ne: 'EASY'}}
+    // }
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats
+      }
+    });
+  
   } catch (err) {
     res.status(404).json({
       status: 'fail',
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,7 +6,13 @@ const router = express.Router();
 //router.param('id', tourController.checkID);
 //router.param('body', tourController.checkBody);
 
-router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours)
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router
+  .route('/tour-stats')
+  .get(tourController.getTourStats);
 
 router
   .route('/')
