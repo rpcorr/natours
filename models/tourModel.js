77e8203c91c3c3ec0e3f4644db4d87c10b006ad6
Ronan Corr <ronan_corr@hotmail.com>
2022-09-27 04:19:45 -0400

    create a virtual property for duration weeks

--- models/tourModel.js
+++ models/tourModel.js
@@ -52,6 +52,16 @@ const tourSchema = new mongoose.Schema({
     select: false
   },
   startDates: [Date]
+},
+{
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }  
+}
+);
+
+// virtual properties
+tourSchema.virtual('durationWeeks').get(function() {
+  return this.duration / 7;
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
