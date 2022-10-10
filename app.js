458a81639442faf66b6862ad2368c3ea266d7d8c
Ronan Corr <ronan_corr@hotmail.com>
2022-10-10 14:03:16 -0400

    handling unhandled routes; remove 'Hello from the middleware'

--- app.js
+++ app.js
@@ -15,11 +15,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware!👋');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -28,5 +23,13 @@ app.use((req, res, next) => {
 // mount the routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  });
+  next();
+});
 
 module.exports = app;
