4b26b74096d066624f4ac1f95ae123175d2ae915
Ronan Corr <ronan_corr@hotmail.com>
2022-10-10 13:16:07 -0400

    FIX: change req.query to this.queryString;

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -35,7 +35,7 @@ class APIFeatures {
   
     limitFields() {
       if (this.queryString.fields) {
-        const fields = req.query.fields.split(',').join(' ');
+        const fields = this.queryString.fields.split(',').join(' ');
         this.query = this.query.select(fields);
       } else {
         this.query = this.query.select('-__v');
