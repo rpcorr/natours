a482b7127f60e616ba84da2648c63105c09d6522
Ronan Corr <ronan_corr@hotmail.com>
2022-11-01 11:49:16 -0400

    setting security HTTP headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,12 +10,17 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// GLOBAL MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
+
+// Set security HTTP headers
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// 100 request from the same IP in 1 hour
+// Limit request from same API 100 request from the same IP in 1 hour
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -22,9 +28,14 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   //console.log(req.headers);
