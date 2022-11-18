0c23f6fba74d5d08c8ac23f58c439823545dd717
Ronan Corr <ronan_corr@hotmail.com>
2022-11-17 19:38:26 -0500

    extending our base template with blocks

--- app.js
+++ app.js
@@ -74,7 +74,19 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.status(200).render('base', {
     tour: 'The Forest Hiker',
-    user: 'Jonas'
+    user: 'Jonas',
+  });
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
   });
 });
 
