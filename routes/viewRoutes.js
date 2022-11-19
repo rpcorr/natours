4e4c8b16244669f85187115e2fb412d69bdbe2fd
Ronan Corr <ronan_corr@hotmail.com>
2022-11-19 08:18:02 -0500

    building the tour page - part 1

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 // mount the routers
 router.get('/', viewController.getOverview);
-router.get('/tour', viewController.getTour);
+router.get('/tour/:slug', viewController.getTour);
 
 module.exports = router;
