c5d32234195b18ae46336e8c1b468c3203011380
Ronan Corr <ronan_corr@hotmail.com>
2022-11-24 02:17:09 -0500

    logging in users with our API part 2

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,9 +4,11 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.use(authController.isLoggedIn)
+
 // mount the routers
 router.get('/', viewsController.getOverview);
-router.get('/tour/:slug', authController.protect, viewsController.getTour);
+router.get('/tour/:slug', viewsController.getTour);
 router.get('/login', viewsController.getLoginForm);
 
 module.exports = router;
