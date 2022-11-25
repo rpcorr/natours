8c988cdb00d87e185d84e364986709f871100ea3
Ronan Corr <ronan_corr@hotmail.com>
2022-11-24 23:45:50 -0500

    logging out users

--- controllers/authController.js
+++ controllers/authController.js
@@ -61,6 +61,16 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    status: 'success',
+  });
+};
+
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting token and check if it's there
   let token;
@@ -103,8 +113,10 @@ exports.protect = catchAsync(async (req, res, next) => {
 });
 
 // Only for rendered pages, no errors!
-exports.isLoggedIn = catchAsync(async (req, res, next) => {
+exports.isLoggedIn = async (req, res, next) => {
   if (req.cookies.jwt) {
+    try {
+
     // 1) verify token
     const decoded = await promisify(jwt.verify)(
       req.cookies.jwt,
@@ -125,9 +137,12 @@ exports.isLoggedIn = catchAsync(async (req, res, next) => {
     // THERE IS A LOGGED IN USER
     res.locals.user = currentUser;
     return next();
+  } catch (err) {
+    return next();
   }
+}
   next();
-});
+};
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
