0ef832debfde2472f3eb32faa04cb290e31784cd
Ronan Corr <ronan_corr@hotmail.com>
2022-11-27 05:06:45 -0500

    building the user account page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,11 +4,10 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.use(authController.isLoggedIn)
-
 // mount the routers
-router.get('/', viewsController.getOverview);
-router.get('/tour/:slug', viewsController.getTour);
-router.get('/login', viewsController.getLoginForm);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+router.get('/me', authController.protect, viewsController.getAccount);
 
 module.exports = router;
